c0e5390ccce83e8038ccb767b9a0171d4875195d
SaadMeddiche <saadmeddiche2004201@gmail.com>
2025-04-16 23:17:41 +0100

    feat: enhance Cell class with grid context and validation methods

--- src/main/java/org/problemSolving/models/Cell.java
+++ src/main/java/org/problemSolving/models/Cell.java
@@ -6,20 +6,35 @@ public class Cell {
 
     public int number = EMPTY;
 
-    public int gridIndex;
+    private final Grid9x9 grid;
 
-    public int squareIndex;
+    public final int gridIndex;
+    public final int squareIndex;
 
-    public final Grid9x9 grid;
+    public final int xGrid;
+    public final int yGrid;
+    public final int xSquare;
+    public final int ySquare;
 
     public Cell(int gridIndex, int squareIndex, Grid9x9 grid) {
+
         this.grid = grid;
+
         this.gridIndex = gridIndex;
         this.squareIndex = squareIndex;
+
+        this.xGrid = gridIndex / Grid9x9.SIZE;
+        this.yGrid = gridIndex % Grid9x9.SIZE;
+        this.xSquare = squareIndex / Square.SIZE;
+        this.ySquare = squareIndex % Square.SIZE;
     }
 
     public boolean isEmpty() {
         return number == EMPTY;
+    }
+
+    public boolean isValid() {
+        return number != EMPTY && grid.isCellValid(xGrid, yGrid, xSquare, ySquare);
     }
 
 }
