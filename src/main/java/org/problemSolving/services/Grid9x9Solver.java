05e8189d9b7080617e03cc2bb84158f534fcbd4f
saadMeddiche <saadmeddiche2004204@gmail.com>
2025-04-16 16:46:58 +0100

    feat: add history tracking for number assignments in Grid9x9Solver

--- src/main/java/org/problemSolving/services/Grid9x9Solver.java
+++ src/main/java/org/problemSolving/services/Grid9x9Solver.java
@@ -1,13 +1,19 @@
 package org.problemSolving.services;
 
+import lombok.AllArgsConstructor;
 import org.problemSolving.models.Grid9x9;
 import org.problemSolving.models.Square;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /*
 * NOTE: this class is not finished yet
 * */
 
 public class Grid9x9Solver {
+
+    private List<HistoryRecord> history = new ArrayList<>();
 
     public void solve(Grid9x9 gridToSolve) {
 
@@ -29,9 +35,10 @@ public class Grid9x9Solver {
                         gridToSolve.get(xGrid, yGrid).get(xSquare, ySquare).number = validNumber;
 
                         if(gridToSolve.isNumberValid(xGrid, yGrid, xSquare, ySquare)) {
+                            history.add(new HistoryRecord(i, j, validNumber));
                             break;
                         } else {
-                            gridToSolve.get(xGrid, yGrid).get(xSquare, ySquare).number = 0;
+                            gridToSolve.get(xGrid, yGrid).get(xSquare, ySquare).number = -1;
                         }
 
                     }
@@ -41,6 +48,13 @@ public class Grid9x9Solver {
             }
         }
 
+    }
+
+    @AllArgsConstructor
+    static class HistoryRecord {
+        int i;
+        int j;
+        int chosenNumber;
     }
 
 }
