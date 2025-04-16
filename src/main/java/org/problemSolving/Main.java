a6f77005ca90dc31b3f9d1f628f2406c10c8850c
saadMeddiche <saadmeddiche2004204@gmail.com>
2025-04-16 10:55:40 +0100

    feat: implement JSON parsing for Sudoku grid initialization

--- src/main/java/org/problemSolving/Main.java
+++ src/main/java/org/problemSolving/Main.java
@@ -1,24 +1,50 @@
 package org.problemSolving;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.problemSolving.models.Grid;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class Main {
 
-    public static void process() {
+    public static void process() throws IOException {
 
         Grid grid = new Grid();
 
-        grid.get(2,1).get(0,2).number = 3;
+        try(InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("soduko-examples.json")){
+
+            JSONObject obj = new JSONObject(new JSONTokener(in));
+
+            JSONArray jsonArray = obj.getJSONArray("sodukos");
+
+            for(Object squareObject :  jsonArray) {
+
+                JSONObject squareJson = (JSONObject) squareObject;
+
+                int xGrid = squareJson.getInt("x");
+                int yGrid = squareJson.getInt("y");
+                int[] numbers = squareJson.getJSONArray("numbers").toList().stream().mapToInt(i -> (int) i).toArray();
+
+                for(int i = 0; i < numbers.length; i++) {
+                    grid.get(xGrid,yGrid).get(i/3,i%3).number = numbers[i];
+                }
+            }
+
+        }
+
+//        grid.get(2,1).get(1,2).number = 3;
 
         System.out.println(grid);
         System.out.println(grid.isValid());
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         LocalDateTime start = LocalDateTime.now();
         process();
